c1f4587e3fa15ae272c3084373ed5960c01e2b05
CharlesWithC <me@charlws.com>
2023-04-13 23:42:29 +0800

    Brand new start!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import './functions/config';
+import { loadConfig } from './functions/config';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [abbr, setAbbr] = useState("N/A");
+
+  useEffect(() => {
+    async function fetchAbbr(){
+      const abbr = (await loadConfig("atm", "hub.atmvtc.com")).config.abbr;
+      setAbbr(abbr);
+    }
+    fetchAbbr();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a new start.
+          Drivers Hub Abbreviation: {abbr}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/functions/config.js
@@ -0,0 +1,10 @@
+// TODO: This should call https://config.chub.page/config?domain=xxx to get the config
+// We will abandon the method of using local config files to improve scability
+
+import axios from "axios";
+
+export const loadConfig = async (abbr, domain) => {
+    const url = `https://config.chub.page/${abbr}/config?domain=${domain}`;
+    const resp = (await axios.get(url)).data;
+    return resp;
+}
