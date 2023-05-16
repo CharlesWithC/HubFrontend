87c51202178284b4e82258db46f0589c41052803
CharlesWithC <me@charlws.com>
2023-05-16 22:41:15 +0800

    Reworked chart template with chartjs

--- src/components/statcard.js
+++ src/components/statcard.js
@@ -1,74 +1,102 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+import { Card, CardContent, Typography } from '@mui/material';
 
-function StatCard() {
-    const series = [
-        {
-            name: 'series1',
-            data: [31, 40, 28, 51, 42, 109, 100]
-        },
-        {
-            name: 'series2',
-            data: [11, 32, 45, 32, 34, 52, 41]
-        }
-    ];
+const StatCard = () => {
+    const chartRef = useRef(null);
 
-    const options = {
-        chart: {
-            height: 350,
-            type: 'area',
-            toolbar: {
-                show: false
+    useEffect(() => {
+        const ctx = chartRef.current.getContext('2d');
+
+        const inputs = {
+            count: 7, // Modify this value as per your requirement
+        };
+
+        const generateLabels = () => {
+            return Array.from({ length: inputs.count }, (_, i) => i + 1);
+        };
+
+        const generateData = () => {
+            return Array.from({ length: inputs.count }, () => Math.random() * 100);
+        };
+
+        const data = {
+            labels: generateLabels(),
+            datasets: [
+                {
+                    label: 'Dataset',
+                    data: generateData(),
+                    borderColor: 'rgba(255, 99, 132, 1)', // Red color
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)', // Red color with transparency
+                    fill: 'start',
+                },
+            ],
+        };
+
+        const options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    display: false, // Hide x-axis
+                },
+                y: {
+                    display: false, // Hide y-axis
+                },
             },
-            zoom: {
-                enabled: false
-            }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            curve: 'smooth'
-        },
-        xaxis: {
-            type: 'datetime',
-            labels: {
-                show: false
+            plugins: {
+                title: {
+                    display: false, // Hide title
+                },
+                legend: {
+                    display: false, // Hide legend
+                },
+                tooltip: {
+                    enabled: false, // Disable tooltip
+                },
             },
-            axisBorder: {
-                show: false
+            layout: {
+                padding: 0, // Remove padding
             },
-            tooltip: {
-                enabled: false
+            elements: {
+                line: {
+                    tension: 0, // Set tension to 0 to remove curved lines
+                },
             },
-            axisTicks: {
-                show: false
-            }
-        },
-        yaxis: {
-            labels: {
-                show: false
+            interaction: {
+                mode: 'index',
+                intersect: false,
             },
-            axisBorder: {
-                show: false
-            }
-        },
-        tooltip: {
-            enabled: false
-        },
-        legend: {
-            show: false
-        },
-        grid: {
-            show: false
-        },
-    };
+        };
+
+        const chart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: options,
+        });
+
+        console.log("re-render");
+
+        return () => {
+            chart.destroy();
+        };
+    }, []);
 
     return (
-        <div id="chart">
-            <ReactApexChart options={options} series={series} type="area" height={350} />
-        </div>
+        <Card>
+            <CardContent>
+                <Typography variant="h6" component="div">
+                    Latest Stat
+                </Typography>
+                <Typography variant="body1" component="div">
+                    xxx drivers
+                </Typography>
+            </CardContent>
+            <div style={{ height: '400px' }}>
+                <canvas ref={chartRef} style={{ height: '100%' }} />
+            </div>
+        </Card>
     );
-}
+};
 
 export default StatCard;
