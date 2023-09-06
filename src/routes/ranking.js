db548d8ca91c7faac86c84294b9ae7c282334100
CharlesWithC <me@charlws.com>
2023-09-06 23:24:49 +0800

    Updated ranking tab to handle new rank type format

--- src/routes/ranking.js
+++ src/routes/ranking.js
@@ -13,6 +13,9 @@ const Ranking = () => {
         setSnackbarContent("");
     }, []);
 
+    const [curRankTypeId, setRankTypeId] = useState(null);
+    const [curRankRoles, setCurRankRoles] = useState([]);
+    const [curRankPointTypes, setCurRankPointTypes] = useState([]);
     const [userPoints, setUserPoints] = useState(null);
     const [rankIdx, setRankIdx] = useState(null);
 
@@ -20,6 +23,17 @@ const Ranking = () => {
         async function doLoad() {
             const loadingStart = new CustomEvent('loadingStart', {});
             window.dispatchEvent(loadingStart);
+
+            if (curRankTypeId === null) {
+                for (let i = 0; i < vars.ranks.length; i++) {
+                    if (vars.ranks[i].default) {
+                        setRankTypeId(vars.ranks[i].id);
+                        setCurRankRoles(vars.ranks[i].details);
+                        setCurRankPointTypes(vars.ranks[i].point_types);
+                        break;
+                    }
+                }
+            }
 
             const [_leaderboard] = await makeRequestsWithAuth([`${vars.dhpath}/dlog/leaderboard?userids=${vars.userInfo.userid}`]);
             if (_leaderboard.list.length === 0) {
@@ -29,25 +43,29 @@ const Ranking = () => {
                 window.dispatchEvent(loadingEnd);
                 return;
             }
-            setUserPoints(_leaderboard.list[0].points.total);
 
-            let points = _leaderboard.list[0].points.total;
-            if (points < vars.ranks[0].points) setRankIdx(-1);
-            for (let i = 0; i < vars.ranks.length - 1; i++) {
-                if (points > vars.ranks[i].points && points < vars.ranks[i + 1].points) {
+            let points = 0;
+            for (let i = 0; i < curRankPointTypes.length; i++) {
+                points += _leaderboard.list[0].points[curRankPointTypes[i]];
+            }
+            setUserPoints(points);
+
+            if (points < curRankRoles[0].points) setRankIdx(-1);
+            for (let i = 0; i < curRankRoles.length - 1; i++) {
+                if (points > curRankRoles[i].points && points < curRankRoles[i + 1].points) {
                     setRankIdx(i);
                 }
             }
-            if (points > vars.ranks[vars.ranks.length - 1].points) setRankIdx(vars.ranks.length - 1);
+            if (points > curRankRoles[curRankRoles.length - 1].points) setRankIdx(curRankRoles.length - 1);
 
             const loadingEnd = new CustomEvent('loadingEnd', {});
             window.dispatchEvent(loadingEnd);
         }
         doLoad();
-    }, []);
+    }, [curRankPointTypes, curRankRoles, curRankTypeId]);
 
     const getDiscordRole = useCallback(async () => {
-        let resp = await axios({ url: `${vars.dhpath}/member/roles/rank`, method: "PATCH", headers: { Authorization: `Bearer ${getAuthToken()}` } });
+        let resp = await axios({ url: `${vars.dhpath}/member/roles/rank/${curRankTypeId}`, method: "PATCH", headers: { Authorization: `Bearer ${getAuthToken()}` } });
         if (resp.status === 204) {
             setSnackbarContent("You received a new rank role!");
             setSnackbarSeverity("success");
@@ -55,7 +73,7 @@ const Ranking = () => {
             setSnackbarContent(resp.data.error);
             setSnackbarSeverity("error");
         }
-    }, []);
+    }, [curRankTypeId]);
 
     return <>
         {userPoints !== null && <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
@@ -66,11 +84,11 @@ const Ranking = () => {
                             Previous Rank
                         </Typography>
                         {rankIdx >= 1 && <>
-                            <Typography variant="h5" align="center" component="div" sx={{ color: vars.ranks[rankIdx - 1]?.color }}>
-                                {vars.ranks[rankIdx - 1].name}
+                            <Typography variant="h5" align="center" component="div" sx={{ color: curRankRoles[rankIdx - 1]?.color }}>
+                                {curRankRoles[rankIdx - 1].name}
                             </Typography>
                             <Typography variant="subtitle2" align="center" sx={{ mt: 1 }}>
-                                {TSep(vars.ranks[rankIdx - 1].points)} Pts | -{TSep(userPoints - vars.ranks[rankIdx - 1].points)} Pts
+                                {TSep(curRankRoles[rankIdx - 1].points)} Pts | -{TSep(userPoints - curRankRoles[rankIdx - 1].points)} Pts
                             </Typography>
                         </>}
                         {rankIdx <= 0 && <>
@@ -91,8 +109,8 @@ const Ranking = () => {
                             Current Rank
                         </Typography>
                         {rankIdx >= 0 && <>
-                            <Typography variant="h5" align="center" component="div" sx={{ color: vars.ranks[rankIdx]?.color, cursor: "pointer" }} onClick={getDiscordRole}>
-                                {vars.ranks[rankIdx].name}
+                            <Typography variant="h5" align="center" component="div" sx={{ color: curRankRoles[rankIdx]?.color, cursor: "pointer" }} onClick={getDiscordRole}>
+                                {curRankRoles[rankIdx].name}
                             </Typography>
                             <Typography variant="subtitle2" align="center" sx={{ mt: 1 }}>
                                 {TSep(userPoints)} Pts
@@ -115,15 +133,15 @@ const Ranking = () => {
                         <Typography variant="subtitle2" align="center" gutterBottom>
                             Next Rank
                         </Typography>
-                        {rankIdx + 1 <= vars.ranks.length - 1 && <>
-                            <Typography variant="h5" align="center" component="div" sx={{ color: vars.ranks[rankIdx + 1]?.color }}>
-                                {vars.ranks[rankIdx + 1].name}
+                        {rankIdx + 1 <= curRankRoles.length - 1 && <>
+                            <Typography variant="h5" align="center" component="div" sx={{ color: curRankRoles[rankIdx + 1]?.color }}>
+                                {curRankRoles[rankIdx + 1].name}
                             </Typography>
                             <Typography variant="subtitle2" align="center" sx={{ mt: 1 }}>
-                                {TSep(vars.ranks[rankIdx + 1].points)} Pts | +{TSep(vars.ranks[rankIdx + 1].points - userPoints)} Pts
+                                {TSep(curRankRoles[rankIdx + 1].points)} Pts | +{TSep(curRankRoles[rankIdx + 1].points - userPoints)} Pts
                             </Typography>
                         </>}
-                        {rankIdx + 1 > vars.ranks.length - 1 && <>
+                        {rankIdx + 1 > curRankRoles.length - 1 && <>
                             <Typography variant="h5" align="center" component="div" sx={{ color: "grey" }}>
                                 N/A
                             </Typography>
@@ -136,7 +154,7 @@ const Ranking = () => {
             </Grid >
         </Grid >}
         <Grid container spacing={2}>
-            {vars.ranks.map((rank) => (
+            {curRankRoles.map((rank) => (
                 <Grid item xs={12} sm={12} md={4} lg={4}>
                     <Card>
                         <CardContent>
