0f76eabd2fff29b087210d059c6330f959ecc160
CharlesWithC <me@charlws.com>
2023-09-18 22:25:21 +0800

    feat: update points + member ctxmenu item icon

--- src/components/usercard.js
+++ src/components/usercard.js
@@ -1,7 +1,10 @@
 import React, { useState, useCallback } from 'react';
-import { Avatar, Chip, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, Button, Snackbar, Alert } from "@mui/material";
+import { Avatar, Chip, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, Button, Snackbar, Alert, Grid, TextField, Typography, ListItemIcon } from "@mui/material";
 import { Portal } from '@mui/base';
 import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPeopleGroup, faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 import RoleSelect from './roleselect';
 
@@ -68,6 +71,20 @@ const UserCard = (props) => {
         setDialogBtnDisabled(false);
     }, [userid, newRoles]);
 
+    const [newPoints, setNewPoints] = useState({ distance: 0, bonus: 0 });
+    const updatePoints = useCallback(async () => {
+        setDialogBtnDisabled(true);
+        let resp = await axios({ url: `${vars.dhpath}/member/${userid}/points`, method: "PATCH", data: { distance: newPoints.distance, bonus: newPoints.bonus }, headers: { Authorization: `Bearer ${getAuthToken()}` } });
+        if (resp.status === 204) {
+            setSnackbarContent("Points updated!!");
+            setSnackbarSeverity("success");
+        } else {
+            setSnackbarContent(resp.data.error);
+            setSnackbarSeverity("error");
+        }
+        setDialogBtnDisabled(false);
+    }, [userid, newPoints]);
+
     let content = <div style={{ display: "inline-block" }} onContextMenu={handleContextMenu}>
         {!useChip && <>
             {!textOnly && <><Avatar src={avatar}
@@ -97,18 +114,51 @@ const UserCard = (props) => {
             open={showContextMenu}
             onClose={(e) => { e.preventDefault(); e.stopPropagation(); setShowContextMenu(false); }}
         >
-            {checkPerm(vars.userInfo.roles, ["admin", "hrm", "hr", "update_member_roles"]) && <MenuItem onClick={(e) => { updateCtxAction(e, "update-roles"); }}>Update Roles</MenuItem>}
+            {userid >= 0 && checkPerm(vars.userInfo.roles, ["admin", "hrm", "hr", "update_member_roles"]) && <MenuItem onClick={(e) => { updateCtxAction(e, "update-roles"); }}><ListItemIcon><FontAwesomeIcon icon={faPeopleGroup} /></ListItemIcon> Update Roles</MenuItem>}
+            {userid >= 0 && checkPerm(vars.userInfo.roles, ["admin", "hrm", "hr", "update_member_points"]) && <MenuItem onClick={(e) => { updateCtxAction(e, "update-points"); }}><ListItemIcon><FontAwesomeIcon icon={faTrophy} /></ListItemIcon> Update Points</MenuItem>}
         </Menu>}
         <div style={{ display: "inline-block" }} onClick={(e) => { e.stopPropagation(); }}>
-            {ctxAction === "update-roles" &&
-                <Dialog open={true} onClose={() => { setCtxAction(""); }} >
-                    <DialogTitle>Update Roles</DialogTitle>
+            {ctxAction === "update-roles" && userid >= 0 &&
+                <Dialog open={true} onClose={() => { setCtxAction(""); }} fullWidth >
+                    <DialogTitle>Update Roles | {name} ({userid})</DialogTitle>
                     <DialogContent>
-                        <RoleSelect label={`${name} (${userid})`} initialRoles={roles} onUpdate={setNewRoles} />
+                        <RoleSelect initialRoles={roles} onUpdate={setNewRoles} />
                     </DialogContent>
                     <DialogActions>
                         <Button variant="primary" onClick={() => { setCtxAction(""); }}>Close</Button>
                         <Button variant="contained" onClick={() => { updateRoles(); }} disabled={dialogBtnDisabled}>Save</Button>
+                    </DialogActions>
+                </Dialog>
+            }
+            {ctxAction === "update-points" && userid >= 0 &&
+                <Dialog open={true} onClose={() => { setCtxAction(""); }} fullWidth  >
+                    <DialogTitle>Update Points | {name} ({userid})</DialogTitle>
+                    <DialogContent>
+                        <Typography variant="body2">- Distance should be given when the tracker fails to submit jobs automatically</Typography>
+                        <Typography variant="body2">- Bonus points could be given as extra reward (e.g. special events / contribution)</Typography>
+                        <Typography variant="body2">- Use negative number to remove points</Typography>
+                        <Grid container spacing={2} sx={{ mt: "5px" }}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Distance (km)"
+                                    value={newPoints.distance}
+                                    onChange={(e) => setNewPoints({ ...newPoints, distance: e.target.value })}
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Bonus Points"
+                                    value={newPoints.bonus}
+                                    onChange={(e) => setNewPoints({ ...newPoints, bonus: e.target.value })}
+                                    fullWidth
+                                />
+                            </Grid>
+                        </Grid>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="primary" onClick={() => { setCtxAction(""); }}>Close</Button>
+                        <Button variant="contained" onClick={() => { updatePoints(); }} disabled={dialogBtnDisabled}>Update</Button>
                     </DialogActions>
                 </Dialog>
             }
