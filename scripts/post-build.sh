cacfd20f5893d61146ebc697aba2148af8467d00
CharlesWithC <me@charlws.com>
2023-09-29 13:47:59 +0800

    feat: postbuild script to automate cdn domain

---
+++ scripts/post-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+replacement="https://cdn.chub.page/beta/static"
+
+# Replace "/beta/static" with the specified URL in index.html
+sed -i "s|/beta/static|$replacement|g" ./build/index.html
+
+# Replace "/beta/static" with the specified URL in asset-manifest.json
+sed -i "s|/beta/static|$replacement|g" ./build/asset-manifest.json
