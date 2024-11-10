c5cb1ee1d09bf9f7a16f9b0a8433b22f5c4dc25d
CharlesWithC <me@charlws.com>
2024-11-10 15:10:49 -0500

    chore: add german translation

--- src/routes/freightmaster.js
+++ src/routes/freightmaster.js
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next';
 import { useContext, useState, useEffect, useCallback, useRef } from "react";
 import { AppContext, CacheContext } from '../context';
 import { useTranslation } from "react-i18next";
